a59ebd9736d90b22e28ed62313eb43faa5c409a4
pollirrata <jorgegaona@yahoo.com>
2022-10-03 17:20:11 -0500

    Modulo 2/Ejercicio 4

---
+++ modulo2/ejercicio4.cs
@@ -0,0 +1,50 @@
+#region Inicializacion
+
+int elementos = 10;
+int[] numeros = new int[elementos];
+char[] letras = {'a','b','c','d','e','f'};
+
+Console.Clear(); //Para limpiar la consola con cada nueva ejecucion
+#endregion
+
+#region For
+
+//1. Ciclo for
+// Console.WriteLine("Arreglo numeros");
+// for(int i = 0; i < elementos; i++){
+//     Console.WriteLine(numeros[i]);
+// }
+// Console.WriteLine("Arreglo letras");
+// for(int i = 0; i < 6; i++){   
+//     Console.WriteLine(letras[i]);
+// }
+
+#endregion
+
+#region ForEach
+
+//2. Ciclo foreach
+
+// foreach(char letra in letras){
+//     Console.WriteLine(letra);
+// }
+
+#endregion
+
+#region While
+int indiceNumero = 0;
+//3. Ciclo while
+// while (numeros[indiceNumero] > 0){
+//     Console.WriteLine("Entramos al while");
+//     Console.WriteLine(numeros[indiceNumero]);
+//      indiceNumero++;
+// }
+
+//4. Ciclo do..while
+// do{
+//     Console.WriteLine("Entramos al do..while");
+//     Console.WriteLine(numeros[indiceNumero]);
+//      indiceNumero++;
+// }while (numeros[indiceNumero] > 0);
+
+#endregion
